456b8e9fa30123d8899cf0a81e013075b66149cd
tolleiv <github@tolleiv.de>
2016-07-06 11:40:43 +0200

    Merge aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,19 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
+  alias ls="gls -hF --color"
   alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -lha --color'
+  alias ll="gls -l --color --group-directories-first"
+  alias la='gls -A --color'
+else
+  alias ls='ls -hF --color'  # add colors for filetype recognition
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color --group-directories-first "
+  alias la='ls -A'          # show hidden files
 fi
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+# -> Prevents accidentally clobbering files.
+alias mkdir='mkdir -p'
